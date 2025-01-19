fc254cfd4a55ea00396db23a2757a6e5fcbb2f5f
Prosenjit Mondol <prosen1156@gmail.com>
2025-01-19 12:37:26 +0600

    1/19/25

--- 1.sql
+++ 1.sql
@@ -1,2 +1,25 @@
 select * from location;
+show user;
+SELECT * from newspaper;
 
+describe newspaper;
+set FEEDBACK OFF;
+
+SELECT * from NEWSPAPER WHERE section='F' ORDER by page,FEATURE;
+
+select Feature, Section, Page from NEWSPAPER
+ where Section = 'F'
+ order by Page desc, Feature;
+
+  select Feature, Section, Page
+ from NEWSPAPER
+ where Page = 6;
+
+
+ select Feature, Section, Page
+ from NEWSPAPER
+ where Section = 'B';
+
+ select Feature, Section, Page
+ from NEWSPAPER
+ where Page > 4;
