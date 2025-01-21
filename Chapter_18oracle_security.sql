9c50cf21cf9937ebc3a25a47fd99bf58cc045bfa
Prosenjit Mondol <prosen1156@gmail.com>
2025-01-21 13:14:44 +0600

    1/21/25

--- Chapter_18oracle_security.sql
+++ Chapter_18oracle_security.sql
@@ -25,4 +25,92 @@ CONNECT dora/psyche;
  GRANT CREATE SESSION to prosen;
  show USER;
  CONNECT prosen/prosen;
- 
+
+ // Password Change
+ show USER;
+ USER is "PROSEN"
+
+  password
+Changing password for PROSEN
+Old password:prosen
+New password:pro
+Retype new password:pro
+Password changed
+
+show user
+USER is "PROSEN"
+SQL> connect system/0000
+Connected.
+SQL> show user
+USER is "SYSTEM"
+SQL> connect prosen/pro;
+Connected.
+SQL> show user
+USER is "PROSEN"
+SQL>
+
+// FAILED LOGIN ATTEMPTS
+
+ show user
+USER is "SYSTEM"
+SQL> create profile limit_profile limit
+  2  failed_login_attempts 3;
+
+Profile created.
+
+SQL> create user jane identified by eyre
+  2  profile limit_profile;
+
+User created.
+
+SQL> show user
+USER is "SYSTEM"
+SQL> grant create session to jane;
+
+Grant succeeded.
+
+SQL> connect jane/eyre;
+Connected.
+SQL> show user
+USER is "JANE"
+SQL> connect system/0000
+Connected.
+SQL> show user
+USER is "SYSTEM"
+SQL> connect jane/eyr;
+ERROR:
+ORA-01017: invalid username/password; logon denied
+
+
+Warning: You are no longer connected to ORACLE.
+SQL> connect jane/eyr;
+ERROR:
+ORA-01017: invalid username/password; logon denied
+
+
+SQL> connect jane/eyr;
+ERROR:
+ORA-01017: invalid username/password; logon denied
+
+
+SQL> connect jane/eyr;
+ERROR:
+ORA-28000: the account is locked
+
+//  account unlock
+
+alter user JANE account unlock;
+User altered.
+SQL> connect jane/eyre;
+Connected.
+SQL> show user
+USER is "JANE"
+
+
+// show profile attribute
+
+SELECT RESOURCE_NAME, LIMIT
+FROM DBA_PROFILES
+WHERE PROFILE = 'LIMITED_PROFILE';
+
+
